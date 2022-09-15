da1f0bff57c5b8dba681685d2c9b201544859eb1
fmd6ms <fmd6ms@I417-16.MIK>
2022-09-15 09:01:39 +0200

    garbage collector

---
+++ ConsoleApp_GC/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp_GC
+{
+    public class BaseClass {
+        public static int counter=0;
+        public BaseClass() {
+            counter++;
+        }
+         ~BaseClass(){
+            counter--;
+            }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int prev = 0;
+            while (true) {
+                int counter = BaseClass.counter;
+                if (counter >= prev)
+                {
+
+                    var c = new BaseClass();
+                    Console.WriteLine(BaseClass.counter);
+                }
+                else {
+                    Console.WriteLine("\n GC: "+counter);
+                    break;
+                }
+                prev = counter;
+            }
+        }
+    }
+}
